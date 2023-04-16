91b3490489ca2713bb48e1b4c4ab1e3734aadb11
Feyalon <63968483+Feyalon@users.noreply.github.com>
2023-04-16 18:42:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const input = document.getElementById("input")
+function searchSubmit(){
+    window.location.replace(`https://www.google.com/search?client=firefox-b-d&q=${input.value}`)
+}
+const bool = false;
